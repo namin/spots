c42043b6a56086d923f8d173eae4e6d063a8d538
Nada Amin <namin@alum.mit.edu>
2008-11-14 11:13:31 -0500

    probabilistic modeling

---
+++ probabilisticModeling/probabilisticModeling.fsx
@@ -0,0 +1,142 @@
+#light
+
+// ----------------------------
+// Listing 9-7.
+
+
+type Distribution<'a> =
+    abstract Sample : 'a
+    abstract Support : Set<'a>
+    abstract Expectation: ('a -> float) -> float
+
+let always x =
+    { new Distribution<'a> with
+         member d.Sample = x
+         member d.Support = Set.singleton x
+         member d.Expectation(H) = H(x) }
+
+let rnd = System.Random()
+
+let coinFlip (p:float) (d1:Distribution<'a>) (d2:Distribution<'a>) =
+    if p < 0.0 || p > 1.0 then failwith "invalid probability in coinFlip"
+    { new Distribution<'a> with
+         member d.Sample =
+             if rnd.NextDouble() < p then d1.Sample else d2.Sample
+         member d.Support = Set.Union(d1.Support,d2.Support)
+         member d.Expectation(H) =
+             p * d1.Expectation(H) + (1.0-p) * d2.Expectation(H) }
+
+// ----------------------------
+// Listing 9-8.
+
+let bind (dist:Distribution<'a>) (k: 'a -> Distribution<'b>) =
+    { new Distribution<'b> with
+         member d.Sample = (k(dist.Sample)).Sample
+         member d.Support = Set.Union(dist.Support.Map(fun d -> (k d).Support))
+         member d.Expectation(H) = dist.Expectation(fun x -> (k x).Expectation(H)) }
+
+type DistributionBuilder() =
+    member x.Delay(f) = bind (always ()) f
+    member x.Let(v,f) = bind (always v) f
+    member x.Bind(d,f) = bind d f
+    member x.Return(x) = always x
+
+let dist = new DistributionBuilder()
+
+// ----------------------------
+// Listing 9-9.
+
+let weightedCases (inp: ('a * float) list) =
+    let rec coinFlips w l =
+        match l with
+        | []          -> failwith "no coinFlips"
+        | [(d,_)]     -> always d
+        | (d,p)::rest -> coinFlip (p/(1.0-w)) (always d) (coinFlips (w+p) rest)
+    coinFlips 0.0 inp
+
+let countedCases inp =
+    let total = List.sumByInt (fun (_,v) -> v) inp
+    weightedCases (inp |> List.map (fun (x,v) -> (x,(float v/float total))))
+
+type Outcome = Even | Odd | Zero
+let roulette = countedCases [ Even,18; Odd,18; Zero,1]
+
+
+// ----------------------------
+
+roulette.Sample
+
+roulette.Sample
+
+roulette.Expectation (function Even -> 10.0 | Odd -> 0.0 | Zero -> 0.0)
+
+// ----------------------------
+
+type Light =
+    | Red
+    | Green
+    | Yellow
+
+let trafficLightD = weightedCases [ Red,0.50; Yellow,0.10; Green, 0.40 ]
+
+type Action = Stop | Drive
+
+let cautiousDriver light =
+    dist { match light with
+           | Red -> return Stop
+           | Yellow -> return! weightedCases [ Stop, 0.9; Drive, 0.1 ]
+           | Green -> return Drive }
+
+let aggressiveDriver light =
+    dist { match light with
+           | Red    -> return! weightedCases [ Stop, 0.9; Drive, 0.1 ]
+           | Yellow -> return! weightedCases [ Stop, 0.1; Drive, 0.9 ]
+           | Green  -> return Drive }
+
+let otherLightFun light =
+    match light with
+    | Red -> Green
+    | Yellow -> Red
+    | Green -> Red
+
+let otherLightDFun light =
+    dist { match light with
+           | Red -> return! weightedCases [ Green, 0.8; Yellow, 0.2 ]
+           | Yellow -> return Red
+           | Green -> return Red }
+    
+type CrashResult = Crash | NoCrash
+
+let crash(driverOneD,driverTwoD,lightD) =
+    dist { // Sample from the traffic light
+           let! light = lightD
+
+           // Sample the first driver's behavior given the traffic light
+           let! driverOne = driverOneD light
+
+           // Sample from the second traffic light
+           let! otherLight = otherLightDFun light
+           
+           // Sample the second driver's behavior given the traffic light
+           let! driverTwo = driverTwoD otherLight
+
+           // Work out the probability of a crash
+           match driverOne, driverTwo with
+             | Drive,Drive -> return! weightedCases [ Crash, 0.9; NoCrash, 0.1 ]
+             | _ -> return NoCrash }
+
+let model = crash(cautiousDriver,aggressiveDriver,trafficLightD)
+
+let model2 = crash(aggressiveDriver,aggressiveDriver,trafficLightD)
+
+// ----------------------------
+
+model.Sample
+
+model.Sample
+
+model.Expectation(function Crash -> 1.0 | NoCrash -> 0.0)
+
+model2.Expectation(function Crash -> 1.0 | NoCrash -> 0.0)
+
+// ----------------------------
---
+++ probabilisticModeling/probabilisticModeling.scala
@@ -0,0 +1,174 @@
+package probabilisticModeling
+
+object probabilisticModeling extends Application {
+  import scala.collection.immutable.Set1
+  import scala.util.Random
+
+  abstract class Distribution[A] {
+    def Sample(): A
+    def Support(): Set[A]
+    def Expectation(H: A => Double): Double
+    def map[B](k: A => B) = flatMap((x: A) => always(k(x)))
+    def flatMap[B](k: A => Distribution[B]): Distribution[B] = bind(this)(k)
+  }
+  
+  class AlwaysDistribution[A](x: A) extends Distribution[A] {
+      def Sample() = x
+      def Support() = new Set1(x)
+      def Expectation(H: A => Double) = H(x)
+  }
+  
+  def always[A](x: A) = new AlwaysDistribution(x)
+  
+  val rnd = new Random
+  
+  
+  class CoinFlipDistribution[A](p: Double)(d1: Distribution[A])(d2: Distribution[A]) extends Distribution[A] {
+    if (p < 0.0 || p > 1.0) error("invalid probability")
+    def Sample() = 
+      if (rnd.nextDouble() < p) d1.Sample() else d2.Sample() 
+    def Support() = 
+      d1.Support() ++ d2.Support()
+    def Expectation(H : A => Double) = 
+      p * d1.Expectation(H) + (1.0-p) * d2.Expectation(H)
+  }
+
+  def coinFlip[A](p: Double)(d1: Distribution[A])(d2: Distribution[A]) = new CoinFlipDistribution(p)(d1)(d2)
+  
+  class BindDistribution[A,B](dist: Distribution[A])(k: A => Distribution[B]) extends Distribution[B] {
+    def Sample() =	
+      (k(dist.Sample())).Sample()
+    def Support() = 
+      dist.Support().flatMap(d => k(d).Support())
+    def Expectation(H : B => Double) = 
+      dist.Expectation(x => k(x).Expectation(H))
+  }
+  
+  def bind[A,B](dist: Distribution[A])(k: A => Distribution[B]): Distribution[B] = new BindDistribution(dist)(k)
+
+  def weightedCases[A](inp: List[(A,Double)]): Distribution[A] = {
+    def coinFlips[A](w: Double)(l: List[(A,Double)]): Distribution[A] = {
+      l match {
+        case Nil => error("no coinFlips")
+        case (d,_)::Nil => always(d)
+        case (d,p)::rest => coinFlip(p/(1.0-w))(always(d))(coinFlips(w+p)(rest))
+      }
+    }
+    coinFlips(0)(inp)
+  }
+ 
+  def countedCases[A](inp: List[(A,Int)]): Distribution[A] = {
+    def sumTotal[A](inp: List[(A,Int)]): Int = 
+      inp match {
+        case Nil => 0
+        case (_,n)::rest => n + sumTotal(rest)
+      }
+    val total = 1.0*(sumTotal(inp))
+    weightedCases(inp map (t => t match { case (x,v) => (x,v/total) }))
+  }
+  
+  abstract class Outcome
+  case class Even() extends Outcome
+  case class Odd() extends Outcome
+  case class Zero() extends Outcome
+  
+  def prettyOutcome (outcome: Outcome) =
+    outcome match {
+      case Even() => "even"
+      case Odd() => "odd"
+      case Zero() => "zero"
+    }
+  val roulette = countedCases(List((Even(),18),(Odd(),18),(Zero(),1)))
+  
+  println("roulette sample: " + roulette.Sample())
+  println("roulette sample: " + roulette.Sample())
+  
+  val roulettePayoff = 
+    roulette.Expectation(x => x match {
+      case Even() => 10.0
+      case Odd() => 0.0
+      case Zero() => 0.0
+    }
+    )
+    
+  println("roulette payoff: " + roulettePayoff)
+
+  abstract class Light
+  case class Red() extends Light
+  case class Green() extends Light
+  case class Yellow() extends Light
+
+  def trafficLightD: Distribution[Light] = weightedCases(List((Red(),0.50),(Yellow(),0.10),(Green(),0.40)))
+  
+  abstract class Action
+  case class Stop() extends Action
+  case class Drive() extends Action
+  
+  def cautiousDriver(light: Light): Distribution[Action] =
+    light match {
+      case Red() => always(Stop())
+      case Yellow() => weightedCases(List((Stop(),0.9),(Drive(),0.1)))
+      case Green() => always(new Drive())
+    }
+  
+  def aggressiveDriver(light: Light): Distribution[Action] =
+    light match {
+      case Red() => weightedCases(List((Stop(),0.9),(Drive(),0.1)))
+      case Yellow() => weightedCases(List((Stop(),0.1),(Drive(),0.9)))
+      case Green() => always(Drive())
+    }
+  
+  def otherLightFun(light: Light): Light =
+    light match {
+      case Red() => Green()
+      case Yellow() => Red()
+      case Green() => Red()
+    }
+  
+  def otherLightDFun(light: Light): Distribution[Light] =
+    light match {
+      case Red() => weightedCases(List((Green(),0.8),(Yellow(),0.2)))
+      case Yellow() => always(Red())
+      case Green() => always(Red())
+    }
+  
+  abstract class CrashResult
+  case class Crash() extends CrashResult
+  case class NoCrash() extends CrashResult
+    
+  def crashExplicit(driverOneD: Light => Distribution[Action])(driverTwoD: Light => Distribution[Action])(lightD: Distribution[Light]): Distribution[CrashResult] =
+    lightD.flatMap(light =>
+      driverOneD(light).flatMap(driverOne =>
+        otherLightDFun(light).flatMap(otherLight =>
+          driverTwoD(otherLight).flatMap(driverTwo =>
+            (driverOne, driverTwo) match {
+              case (Drive(),Drive()) => weightedCases(List((Crash(),0.9),(NoCrash(),0.1)))
+              case _ => always(NoCrash())
+            }))))
+  
+  def crash(driverOneD: Light => Distribution[Action])(driverTwoD: Light => Distribution[Action])(lightD: Distribution[Light]): Distribution[CrashResult] =
+    for (light <- lightD;
+         driverOne <- driverOneD(light);
+         otherLight <- otherLightDFun(light);
+         driverTwo <- driverTwoD(otherLight);
+         caseBothDrive <- weightedCases(List((Crash(),0.9),(NoCrash(),0.1)))) yield
+    (driverOne,driverTwo) match {
+      case (Drive(),Drive()) => caseBothDrive
+      case _ => NoCrash()
+    }
+
+  val model = crash(cautiousDriver)(aggressiveDriver)(trafficLightD)    
+  val model2 = crash(aggressiveDriver)(aggressiveDriver)(trafficLightD)
+  
+  println("model sample: " + model.Sample())
+  println("model2 sample: " + model2.Sample())
+  
+  def H(x: CrashResult) = x match {
+    case Crash() => 1.0
+    case NoCrash() => 0.0
+  }
+  
+  println("model crash expectation:" + model.Expectation(H))
+  println("model2 crash expectation:" + model2.Expectation(H))
+
+}
