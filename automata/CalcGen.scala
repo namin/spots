1becee78ad61722a0e19f90a75e9a24b010ba1a7
Nada Amin <namin@alum.mit.edu>
2011-08-23 13:42:37 -0400

    comments

--- automata/CalcGen.scala
+++ automata/CalcGen.scala
@@ -7,7 +7,7 @@ object CalcGen {
   val intType = "L" + intClass + ";"
 
   import cafebabe._
-  import AbstractByteCodes._
+  import AbstractByteCodes._ // augmented with JustNew and CheckCast
   import ByteCodes._
 
   abstract class Fun {
